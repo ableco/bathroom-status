d25268f6dd67cec18ed191a12bedb80080663f73
rickyc <rickyc.us@gmail.com>
2015-06-23 13:51:49 -0400

    adding as of X minutes ago

--- web.rb
+++ web.rb
@@ -1,22 +1,30 @@
 require 'sinatra'
 require 'open-uri'
 require 'json'
+require 'action_view'
+require 'action_view/helpers'
+include ActionView::Helpers::DateHelper
+
 
 set :public_folder, Proc.new { File.join(root, "public") }
 
 get '/' do
-  @status = getStatus
+  @status, @date = last_entry['status'], time_ago
   erb :index
 end
 
 get '/index.json' do
-  getStatus
+  "#{getLastEntry['status']} as of #{time_ago} ago"
 end
 
 
 # Private
+#
+def time_ago
+  time_ago_in_words(Time.parse(last_entry['timestamp']))
+end
 
-def getStatus
+def last_entry
   json = JSON.parse(open("https://data.sparkfun.com/output/JxxL7ZLvaMSNMJyOxLYW.json").read)
-  json[0]['status']
+  json[0]
 end
